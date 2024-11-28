807b578cb72305add485c9e7ee423c83c87007b7
Shmuel Batoniashvili <krihelis@gmail.com>
2024-11-27 19:14:32 -0700

    Add files via upload

--- indexScripts.js
+++ indexScripts.js
@@ -1,1 +1,42 @@
+let slideIndex = 1;
+showSlides(slideIndex);
 
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+function toggleMenu() {
+    const menu = document.querySelector('.menu-list');
+    menu.classList.toggle('show');
+  }
+  
+
+  document.getElementById('quoteForm').addEventListener('submit', function (event) {
+    // Simple validation to check if required fields are filled
+    var form = event.target;
+    if (!form.name.value || !form.email.value || !form.service.value) {
+        alert('Please fill in all required fields.');
+        event.preventDefault(); // Prevent form submission
+    }
+});
